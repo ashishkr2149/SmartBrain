870d6c71a2a97c6ca3c85beee0f2b3e6fec5074f
Ashish <ashishkr.2149@gmail.com>
2024-04-10 08:09:06 +0530

    fixing bugs

--- smartbrain-api/server.js
+++ smartbrain-api/server.js
@@ -7,93 +7,97 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const database = {
-	users: [
-		{
-			id : "123",
-			name : "Ashish",
-			email : "ashishkr.2149@gmail.com",
-			password : "ashishkr.2149",
-			entries : 0,
-			joined : new Date()
-		},
-		{
-			id : "124",
-			name : "Rishu",
-			email : "aspk0711@gmail.com",
-			password : "Ashish@2149",
-			entries : 0,
-			joined : new Date()
-		}
-	]
-}
+// const database = {
+// 	users: [
+// 		{
+// 			id : "123",
+// 			name : "Ashish",
+// 			email : "ashishkr.2149@gmail.com",
+// 			password : "ashishkr.2149",
+// 			entries : 0,
+// 			joined : new Date()
+// 		},
+// 		{
+// 			id : "124",
+// 			name : "Rishu",
+// 			email : "aspk0711@gmail.com",
+// 			password : "Ashish@2149",
+// 			entries : 0,
+// 			joined : new Date()
+// 		}
+// 	]
+// }
 
-app.get('/', (req, res) => {
-	res.send(database.users);
-})
+// app.get('/', (req, res) => {
+// 	res.send(database.users);
+// })
 
-app.post('/signin', (req, res) => {
+// app.post('/signin', (req, res) => {
 
-		if(database.users[0].email === req.body.email && database.users[0].password === req.body.password){
-			res.status(200).json(database.users[0]);
-		}
-		else{
-			res.status(400).json("Error while Signing In");
-		}
+// 		if(database.users[0].email === req.body.email && database.users[0].password === req.body.password){
+// 			res.status(200).json(database.users[0]);
+// 		}
+// 		else{
+// 			res.status(400).json("Error while Signing In");
+// 		}
 
-})
+// })
 
-app.post('/register', (req, res) => {
+// app.post('/register', (req, res) => {
 
-	const { email, name, password } = req.body;
+// 	const { email, name, password } = req.body;
 
-	// bcrypt.hash(password, null, null, (err,hash) => {
-	// 	console.log(hash);
-	// })
+// 	// bcrypt.hash(password, null, null, (err,hash) => {
+// 	// 	console.log(hash);
+// 	// })
 
-	database.users.push({
-		id : "125",
-		name : name,
-		email : email,
-		password : password,
-		entries : 0,
-		joined : new Date()
-	})
+// 	database.users.push({
+// 		id : "125",
+// 		name : name,
+// 		email : email,
+// 		password : password,
+// 		entries : 0,
+// 		joined : new Date()
+// 	})
 
-	res.send(database.users[database.users.length-1]);
-})
+// 	res.send(database.users[database.users.length-1]);
+// })
 
-app.get('/profile/:id', (req, res) => {
+// app.get('/profile/:id', (req, res) => {
 
-	const { id } = req.params;
-	let found = false;
+// 	const { id } = req.params;
+// 	let found = false;
 
-	database.users.forEach((user) => {
-		if(user.id === id){
-			found = true;
-			return res.json(user);
-		}
-	})
-	if(!found){
-		res.status(404).json("Profile not Found.")
-	}
-})
+// 	database.users.forEach((user) => {
+// 		if(user.id === id){
+// 			found = true;
+// 			return res.json(user);
+// 		}
+// 	})
+// 	if(!found){
+// 		res.status(404).json("Profile not Found.")
+// 	}
+// })
 
-app.put('/image', (req, res) => {
+// app.put('/image', (req, res) => {
 
-	const { id } = req.body;
-	let found = false;
+// 	const { id } = req.body;
+// 	let found = false;
 
-	database.users.forEach((user) => {
-		if(user.id === id){
-			found = true;
-			user.entries++;
-			return res.json(user.entries);
-		}
-	})
-	if(!found){
-		res.status(404).json("Profile not Found.")
-	}
+// 	database.users.forEach((user) => {
+// 		if(user.id === id){
+// 			found = true;
+// 			user.entries++;
+// 			return res.json(user.entries);
+// 		}
+// 	})
+// 	if(!found){
+// 		res.status(404).json("Profile not Found.")
+// 	}
+// })
+
+app.use('/', (req, res) => {
+	res.send("Server is running");
 })
 
 app.listen(3000,()=>{
